039e06cdd0f3c620da023b3fe01174a6f47751b0
Aaron Meyer <a@ameyer.me>
2019-08-03 11:22:10 -0700

    Fixed test.

--- test/model.jl
+++ test/model.jl
@@ -1,37 +1,35 @@
-
-
-
+using gcSolver
 
 # Assert the conservation of species throughout the experiment.
-function assertConservation(y, y0, IDX)
-        [1, 4, 5, 7, 8, 11, 12, 14, 15]),  # IL2Rb
-    np.array([0, 3, 5, 6, 8]),  # IL2Ra
-    np.array([9, 10, 12, 13, 15]),  # IL15Ra
-    np.array([16, 17, 18]),  # IL7Ra
-    np.array([19, 20, 21]),  # IL9R
-    np.array([22, 23, 24]),  # IL4Ra
-    np.array([25, 26, 27]),  # IL21Ra
-    np.array([2, 6, 7, 8, 13, 14, 15, 18, 21, 24, 27]), #gc
+function assertConservation(y)
+    # These are all 0-indexed, so add 1
+    consList = [[1, 4, 5, 7, 8, 11, 12, 14, 15],  # IL2Rb
+                [0, 3, 5, 6, 8],  # IL2Ra
+                [9, 10, 12, 13, 15],  # IL15Ra
+                [16, 17, 18],  # IL7Ra
+                [19, 20, 21],  # IL9R
+                [22, 23, 24],  # IL4Ra
+                [25, 26, 27],  # IL21Ra
+                [2, 6, 7, 8, 13, 14, 15, 18, 21, 24, 27]] #gc
 
-        species_delta = y - y0
-
-        # Check for conservation of species sum
-        self.assertAlmostEqual(np.sum(species_delta[IDX]), 0.0, msg=str(IDX))
+    # Check for conservation of species sum
+    for ii in range(1, stop=length(consList))
+        diff = sum(y[consList[ii] .+ 1])
+        @test isapprox(diff, 0.0, atol=1.0e-12) "$ii species not conserved: $diff"
+    end
 end
 
 
-
-###
-# In the absence of trafficking, mass balance should hold in both compartments.
-rxntfR = self.rxntfR.copy()
-rxntfR[17:30] = 0.0
-
-dy = gcSolver.fullModel(y0, 0.0, rxntfR)
-
-# Check for conservation of each surface receptor
-assertConservation(dy, 0.0, idxs)
-
-# Check for conservation of each endosomal receptor
-assertConservation(dy, 0.0, idxs + 28)
-
-
+@testset "Full model mass conservation." begin
+    rxntfR = copy(rxntfR)
+    rxntfR[18:end] .= 0.0
+    y0 = ones(gcSolver.Nspecies)
+    dy = ones(gcSolver.Nspecies)
+    
+    gcSolver.fullDeriv(dy, y0, rxntfR, 0.0)
+    
+    # Check for conservation of each surface receptor
+    assertConservation(dy)
+    # Check for conservation of each endosomal receptor
+    assertConservation(dy[gcSolver.halfL+1:end])
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,27 +5,24 @@ using ForwardDiff
 using BenchmarkTools
 using gcSolver
 
-### Check that runCkine can run
-params = [rand(LogNormal(0.1, 0.25)) for i=1:gcSolver.Nparams]
-params[20] = tanh(params[20])
+rxntfR = [rand(LogNormal(0.1, 0.25)) for i=1:gcSolver.Nparams]
+rxntfR[20] = tanh(rxntfR[20])
 
+include("model.jl")
+
+### Check that runCkine can run
 IL2params = [rand(LogNormal(0.1, 0.25)) for i=1:gcSolver.NIL2params]
 
 tps = [0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0]
 
-#@benchmark runCkine(tps, params, false)
+#@benchmark runCkine(tps, rxntfR, false)
 
-out = runCkine(tps, params, false)
-outTwo = runCkine(tps, params, false)
+out = runCkine(tps, rxntfR, false)
+outTwo = runCkine(tps, rxntfR, false)
 
 @assert out == outTwo
 
 
-fout = ForwardDiff.jacobian(x -> runCkine(tps, x, false)[1], params)
-
-#print(out[1])
-#print("\n\n\n\n")
-
-#print(fout)
+fout = ForwardDiff.jacobian(x -> runCkine(tps, x, false)[1], rxntfR)
 
 ## Run two times and check solutions are identical with/without sensitivity, pretreatment, IL2param
