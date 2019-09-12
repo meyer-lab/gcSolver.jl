4ddbbcaa0ce13b8b14c73da6d7128f7337345fce
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2019-09-11 21:59:07 -0700

    Update gcSolver.jl

--- src/gcSolver.jl
+++ src/gcSolver.jl
@@ -101,7 +101,7 @@ function runCkine(tps::Array{Float64,1}, params::Vector)
         method = Rosenbrock23()
     end
 
-    sol = solve(prob, method; reltol=1.0e-5, abstol=1.0e-5)
+    sol = solve(prob, method; reltol=1.0e-8, abstol=1.0e-8)
 
     solut = sol(tps).u
 
