b7e2ae514ecbd47b65f2c46d376680b82cf7bb4e
aarmey <aarmey@users.noreply.github.com>
2020-01-02 00:07:20 +0000

    Format .jl files

--- src/reaction.jl
+++ src/reaction.jl
@@ -20,29 +20,45 @@ function dYdT(du, u, p, ILs)
     # IL15Ra, IL15_IL15Ra, IL15_IL2Rb, IL15_IL15Ra_IL2Rb, IL15_IL15Ra_gc, IL15_IL2Rb_gc, IL15_IL15Ra_IL2Rb_gc
 
     # IL2/15
-    for i in 0:1
-        pp = view(p, 1 + i*6:8 + i*6)
+    for i = 0:1
+        pp = view(p, (1 + i * 6):(8 + i * 6))
         k12rev = pp[2] * pp[7] / pp[3] # Detailed balance
         k8rev = pp[6] * k12rev / pp[5] # Detailed balance
         k9rev = pp[6] * pp[7] / pp[4] # Detailed balance
 
-        du[2] += -kfbnd * u[2] * ILs[i+1] + pp[3] * u[5 + 7*i] - p[1] * u[2] * u[7 + 7*i] + k9rev * u[9 + 7*i] - p[1] * u[2] * u[4 + 7*i] + pp[7] * u[6 + 7*i];
-        du[3] += -p[1] * u[5 + 7*i] * u[3] + p[5 + 7*i] * u[8 + 7*i] - p[1] * u[4 + 7*i] * u[3] + p[4 + 7*i] * u[7 + 7*i] - p[1] * u[6 + 7*i] * u[3] + p[6 + 7*i] * u[9 + 7*i];
-        du[1 + 9*i] = -kfbnd * u[1 + 9*i] * ILs[i+1] + pp[2] * u[4 + 7*i] - p[1] * u[1 + 9*i] * u[8 + 7*i] + k8rev * u[9 + 7*i] - p[1] * u[1 + 9*i] * u[5 + 7*i] + k12rev * u[6 + 7*i];
-        du[4 + 7*i] = -p[1] * u[4 + 7*i] * u[2] + pp[7] * u[6 + 7*i] - p[1] * u[4 + 7*i] * u[3] + pp[4] * u[7 + 7*i] + kfbnd * ILs[i+1] * u[1 + 9*i] - pp[2] * u[4 + 7*i]
-        du[5 + 7*i] = -p[1] * u[5 + 7*i] * u[1 + 9*i] + k12rev * u[6 + 7*i] - p[1] * u[5 + 7*i] * u[3] + p[5 + 7*i] * u[8 + 7*i] + kfbnd * ILs[i+1] * u[2] - pp[3] * u[5 + 7*i]
-        du[6 + 7*i] = -p[1] * u[6 + 7*i] * u[3] + pp[6] * u[9 + 7*i] + p[1] * u[4 + 7*i] * u[2] - pp[7] * u[6 + 7*i] + p[1] * u[5 + 7*i] * u[1 + 9*i] - k12rev * u[6 + 7*i]
-        du[9 + 7*i] = p[1] * u[8 + 7*i] * u[1 + 9*i] - k8rev * u[9 + 7*i] + p[1] * u[7 + 7*i] * u[2] - k9rev * u[9 + 7*i] + p[1] * u[6 + 7*i] * u[3] - pp[6] * u[9 + 7*i]
-        du[7 + 7*i] = -p[1] * u[7 + 7*i] * u[2] + k9rev * u[9 + 7*i] + p[1] * u[4 + 7*i] * u[3] - pp[4] * u[7 + 7*i]
-        du[8 + 7*i] = -p[1] * u[8 + 7*i] * u[1 + 9*i] + k8rev * u[9 + 7*i] + p[1] * u[3] * u[5 + 7*i] - pp[5] * u[8 + 7*i]
+        du[2] +=
+            -kfbnd * u[2] * ILs[i + 1] + pp[3] * u[5 + 7 * i] - p[1] * u[2] * u[7 + 7 * i] + k9rev * u[9 + 7 * i] - p[1] * u[2] * u[4 + 7 * i] +
+            pp[7] * u[6 + 7 * i]
+        du[3] +=
+            -p[1] * u[5 + 7 * i] * u[3] + p[5 + 7 * i] * u[8 + 7 * i] - p[1] * u[4 + 7 * i] * u[3] + p[4 + 7 * i] * u[7 + 7 * i] -
+            p[1] * u[6 + 7 * i] * u[3] + p[6 + 7 * i] * u[9 + 7 * i]
+        du[1 + 9 * i] =
+            -kfbnd * u[1 + 9 * i] * ILs[i + 1] + pp[2] * u[4 + 7 * i] - p[1] * u[1 + 9 * i] * u[8 + 7 * i] + k8rev * u[9 + 7 * i] -
+            p[1] * u[1 + 9 * i] * u[5 + 7 * i] + k12rev * u[6 + 7 * i]
+        du[4 + 7 * i] =
+            -p[1] * u[4 + 7 * i] * u[2] + pp[7] * u[6 + 7 * i] - p[1] * u[4 + 7 * i] * u[3] +
+            pp[4] * u[7 + 7 * i] +
+            kfbnd * ILs[i + 1] * u[1 + 9 * i] - pp[2] * u[4 + 7 * i]
+        du[5 + 7 * i] =
+            -p[1] * u[5 + 7 * i] * u[1 + 9 * i] + k12rev * u[6 + 7 * i] - p[1] * u[5 + 7 * i] * u[3] +
+            p[5 + 7 * i] * u[8 + 7 * i] +
+            kfbnd * ILs[i + 1] * u[2] - pp[3] * u[5 + 7 * i]
+        du[6 + 7 * i] =
+            -p[1] * u[6 + 7 * i] * u[3] + pp[6] * u[9 + 7 * i] + p[1] * u[4 + 7 * i] * u[2] - pp[7] * u[6 + 7 * i] +
+            p[1] * u[5 + 7 * i] * u[1 + 9 * i] - k12rev * u[6 + 7 * i]
+        du[9 + 7 * i] =
+            p[1] * u[8 + 7 * i] * u[1 + 9 * i] - k8rev * u[9 + 7 * i] + p[1] * u[7 + 7 * i] * u[2] - k9rev * u[9 + 7 * i] +
+            p[1] * u[6 + 7 * i] * u[3] - pp[6] * u[9 + 7 * i]
+        du[7 + 7 * i] = -p[1] * u[7 + 7 * i] * u[2] + k9rev * u[9 + 7 * i] + p[1] * u[4 + 7 * i] * u[3] - pp[4] * u[7 + 7 * i]
+        du[8 + 7 * i] = -p[1] * u[8 + 7 * i] * u[1 + 9 * i] + k8rev * u[9 + 7 * i] + p[1] * u[3] * u[5 + 7 * i] - pp[5] * u[8 + 7 * i]
     end
 
-    for i in 0:3
-        ij = 17 + i*3
-        du[3] += - p[1] * u[3] * u[ij+1] + p[15 + 2*i] * u[ij+2];
-        du[ij] = -kfbnd * u[ij] * ILs[3 + i] + p[14 + 2*i] * u[ij+1];
-        du[ij+1] = kfbnd * u[ij] * ILs[3 + i] - p[14 + 2*i] * u[ij+1] - p[1] * u[3] * u[ij+1] + p[15 + 2*i] * u[ij+2];
-        du[ij+2] = p[1] * u[3] * u[ij+1] - p[15 + 2*i] * u[ij+2];
+    for i = 0:3
+        ij = 17 + i * 3
+        du[3] += -p[1] * u[3] * u[ij + 1] + p[15 + 2 * i] * u[ij + 2]
+        du[ij] = -kfbnd * u[ij] * ILs[3 + i] + p[14 + 2 * i] * u[ij + 1]
+        du[ij + 1] = kfbnd * u[ij] * ILs[3 + i] - p[14 + 2 * i] * u[ij + 1] - p[1] * u[3] * u[ij + 1] + p[15 + 2 * i] * u[ij + 2]
+        du[ij + 2] = p[1] * u[3] * u[ij + 1] - p[15 + 2 * i] * u[ij + 2]
     end
     return nothing
 end
@@ -53,25 +69,25 @@ function fullModel(du, u, pSurf, pEndo, trafP, ILs)
 
     # Calculate cell surface and endosomal reactions
     dYdT(du, u, pSurf, ILs)
-    dYdT(view(du, halfL+1:2*halfL), view(u, halfL+1:2*halfL), pEndo, view(u, halfL*2 + 1:Nspecies))
+    dYdT(view(du, (halfL + 1):(2 * halfL)), view(u, (halfL + 1):(2 * halfL)), pEndo, view(u, (halfL * 2 + 1):Nspecies))
 
     # Handle endosomal ligand balance.
     # Must come before trafficking as we only calculate this based on reactions balance
-    du[57] = -sum(view(du,  halfL+4:halfL+9 )) / internalV
-    du[58] = -sum(view(du, halfL+11:halfL+16)) / internalV
-    du[59] = -sum(view(du, halfL+18:halfL+19)) / internalV
-    du[60] = -sum(view(du, halfL+21:halfL+22)) / internalV
-    du[61] = -sum(view(du, halfL+24:halfL+25)) / internalV
-    du[62] = -sum(view(du, halfL+27:halfL+28)) / internalV
+    du[57] = -sum(view(du, (halfL + 4):(halfL + 9))) / internalV
+    du[58] = -sum(view(du, (halfL + 11):(halfL + 16))) / internalV
+    du[59] = -sum(view(du, (halfL + 18):(halfL + 19))) / internalV
+    du[60] = -sum(view(du, (halfL + 21):(halfL + 22))) / internalV
+    du[61] = -sum(view(du, (halfL + 24):(halfL + 25))) / internalV
+    du[62] = -sum(view(du, (halfL + 27):(halfL + 28))) / internalV
 
     # Actually calculate the trafficking
-    for ii in range(1, stop=halfL)
+    for ii in range(1, stop = halfL)
         if findfirst(isequal(ii), SVector(8, 9, 15, 16, 19, 22, 25, 28)) != nothing
             du[ii] -= u[ii] * (trafP[1] + trafP[2]) # Endo
-            du[ii+halfL] += u[ii] * (trafP[1] + trafP[2]) / internalFrac - trafP[5] * u[ii+halfL] # Endo, deg
+            du[ii + halfL] += u[ii] * (trafP[1] + trafP[2]) / internalFrac - trafP[5] * u[ii + halfL] # Endo, deg
         else
-            du[ii] += -u[ii] * trafP[1] + trafP[4] * (1.0 - trafP[3]) * u[ii+halfL] * internalFrac # Endo, recycling
-            du[ii+halfL] += u[ii]*trafP[1]/internalFrac - trafP[4]*(1.0 - trafP[3])*u[ii+halfL] - (trafP[5]*trafP[3])*u[ii+halfL] # Endo, rec, deg
+            du[ii] += -u[ii] * trafP[1] + trafP[4] * (1.0 - trafP[3]) * u[ii + halfL] * internalFrac # Endo, recycling
+            du[ii + halfL] += u[ii] * trafP[1] / internalFrac - trafP[4] * (1.0 - trafP[3]) * u[ii + halfL] - (trafP[5] * trafP[3]) * u[ii + halfL] # Endo, rec, deg
         end
     end
 
@@ -79,7 +95,7 @@ function fullModel(du, u, pSurf, pEndo, trafP, ILs)
     du[recIDX] += view(trafP, 6:13)
 
     # Degradation does lead to some clearance of ligand in the endosome
-    du[halfL*2 + 1:end] -= view(u, halfL*2 + 1:Nspecies) .* trafP[5]
+    du[(halfL * 2 + 1):end] -= view(u, (halfL * 2 + 1):Nspecies) .* trafP[5]
 
     return nothing
 end
@@ -103,7 +119,7 @@ function solveAutocrine(r)
     # Assuming no autocrine ligand, so can solve steady state
     # Add the species
     y0[recIDX .+ halfL] = r[6:end] / kDeg / internalFrac
-    y0[recIDX] = (r[6:end] + kRec*y0[recIDX .+ halfL] * internalFrac) / r[1]
+    y0[recIDX] = (r[6:end] + kRec * y0[recIDX .+ halfL] * internalFrac) / r[1]
 
     return y0
 end
