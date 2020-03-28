b1385e5246dd0ddca107610e88544b28ee8e5462
Aaron Meyer <a@ameyer.me>
2020-03-28 11:36:30 -0700

    Rm Random

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,8 @@ using Test
 using gcSolver
 using ForwardDiff
 using LinearAlgebra
-using Random
-Random.seed!(123)
 
-rxntfR = exp.(randn(gcSolver.Nparams))
-rxntfR[44] = tanh(rxntfR[44])
+rxntfR = ones(gcSolver.Nparams) * 0.1
 
 tps = [0.0, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0]
 
