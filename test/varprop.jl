d31ed608597a70f1fae1498b4c19eabdeef9e15c
Aaron Meyer <aaron@asmlab.org>
2020-03-27 20:39:52 -0700

    Add variance propagation.

---
+++ test/varprop.jl
@@ -0,0 +1,11 @@
+
+@testset "Reasonable return from varprop function." begin
+	sigma = Matrix{Int}(I, length(rxntfR), length(rxntfR))
+
+    retval = gcSolver.runCkineVarPorp(tps, rxntfR, sigma)
+
+    @time gcSolver.runCkineVarPorp(tps, rxntfR, sigma)
+
+    @test size(retval) == (length(tps), length(tps))
+end
+
