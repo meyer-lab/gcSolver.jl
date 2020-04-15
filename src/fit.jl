85c94c1e6b31121a0326ab2bc8794de45c44e3d4
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2020-04-14 17:08:48 -0700

    UnkVecConst

--- src/fit.jl
+++ src/fit.jl
@@ -17,6 +17,25 @@ const internalV = 623.0 # Same as that used in TAM model
 # p[5:8] is k5rev, k10rev, k11rev, k13rev
 # p[9:12] is k14rev, k16rev, k17rev, k22rev
 # p[13:14] is k23rev, k24rev
+
+function getUnkVec():
+    """Creates full vector of unknown values to be fit"""
+    #kfwd, k4, k5, k16, k17, k22, k23, k27, endo, aendo, sort, krec, kdeg, k34, k35, k36, k37, k38, k39
+    unkVecF = zeros(Float64, 1, 19)
+
+    unkVecF[1] = 0.00125
+    unkVecF[2:7] = 0.679
+    unkVecF[8] = 1.0
+    unkVecF[9] = 0.1
+    unkVecF[10] = 0.678
+    unkVecF[11] = 0.1
+    unkVecF[12] = 0.01
+    unkVecF[13] = 0.13
+    unkVecF[14:19] = 0.679
+
+    return unkVecF
+end
+
 """
     kfbnd = 0.60
     rd["IL2"], rd["IL15"], rd["IL7"], rd["IL9"], rd["IL4"], rd["IL21"], rd["kfwd"] = tuple(rxntfr[0:7])
@@ -34,33 +53,32 @@ const internalV = 623.0 # Same as that used in TAM model
     rd["surf.k25rev"] = kfbnd * 59.0
     rd["surf.k27rev"] = rxntfr[13]
 """
-function getUnkVec():
-    """Creates full vector of unknown values to be fit"""
-    #kfwd, k4, k5, k16, k17, k22, k23, k27, endo, aendo, sort, krec, kdeg, k34, k35, k36, k37, k38, k39
-    unkVecF = zeros(Float64, 1, 19)
-    
-    unkVecF[1] = 0.00125
-    unkVecF[2:7] = 0.679
-    unkVecF[8] = 1.0
-    unkVecF[9] = 0.1
-    unkVecF[10] = 0.678
-    unkVecF[11] = 0.1
-    unkVecF[12] = 0.01
-    unkVecF[13] = 0.13
-    unkVecF[14:19] = 0.679
-    
-    
-
-end
-    
 
 function fitParams(ILs, unkVec, recAbundances):
     """Takes in full unkvec and constructs it into full fit parameters vector"""
     kfbnd = 0.60
     paramvec = zeros(Float64, 1, Nparams)
-    paramvec[1] = 
-    
-    paramvec
+    paramvec[1:3] = ILs
+    paramvec[4] = unkVec[1] #kfwd
+    paramvec[5] = kfbnd * 10.0 #k1rev
+    paramvec[6] = kfbnd * 144.0 #k2rev
+    paramvec[7] = unkVec[2] #k4rev
+    paramvec[8] = unkVec[3] #k5rev
+    paramvec[9] = 12.0 * paramvec[8] / 1.5 #k10
+    paramvec[10] = 63.0 * paramvec[8] / 1.5 #k11
+    paramvec[11] = kfbnd * 0.065 #k13
+    paramvec[12] = kfbnd * 438.0 #k14
+    paramvec[13] = unkVec[4] #k16
+    paramvec[14] = unkVec[5] #k16
+    paramvec[15] = unkVec[6] #k17
+    paramvec[16] = unkVec[7] #k22
+    paramvec[17] = kfbnd * 59.0 #k23
+    paramvec[18] = unkVec[8] #k25
+    paramvec[19] = 5.0 #endoadjust
+    paramvec[20:24] = unkVec[9:13]
+    paramvec[25:29] = receptorExp(recAbundances)
+    paramvec[30] = 0.0#error
+    paramVec[31:36] = unkVec[14:19]
     
 end
 
