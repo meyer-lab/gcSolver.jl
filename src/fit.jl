daf8bf068b2bff6a215f397aea6c2185905c4e98
Brian Orcutt-Jahns <46577511+borcuttjahns@users.noreply.github.com>
2020-10-09 17:52:42 -0700

    Fit fix (#184)
    * Lock in Farhat, small fixes
    
    * Up to Date
    
    * Update Packages
    
    * Fixed Package
    
    * Update gcSolver.jl
    
    * Exclude IL15
    
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>

--- src/fit.jl
+++ src/fit.jl
@@ -10,16 +10,16 @@ function getUnkVec()
     p = 0.1ones(23)
 
     p[1] = 0.001 # means of prior distributions from gc-cytokines paper
-    p[8] = 1.0
+    p[8] = 1.0 #27
     p[10] = 0.678
     p[11] = 0.2
     p[12] = 0.01
     p[13] = 0.13
-    p[14] = 2.0 # initial Treg stat
-    p[15] = 0.5 # initial Thelp stat
-    p[16] = 0.2 # initial NK stat
-    p[17] = 0.2 # initial CD8 stat
-    p[18:23] .= 0.001 # pSTAT Rates
+    p[14] = 5.0 # initial Treg stat
+    p[15] = 0.2 # initial Thelp stat
+    p[16] = 0.3 # initial NK stat
+    p[17] = 0.1 # initial CD8 stat
+    p[18:23] .= 0.001
 
     return p
 end
@@ -28,43 +28,45 @@ end
 """ Takes in full unkvec and constructs it into full fit parameters vector - TODO move this. """
 function fitParams(ILs, unkVec::Vector{T}, recAbundances, CellType::String) where {T}
     kfbnd = 0.60
+    farhatVec = getFarhatVec()
     paramvec = zeros(T, Nparams)
     paramvec[1:3] = ILs
-    paramvec[4] = unkVec[1] #kfwd
+    paramvec[4] = farhatVec[1] #unkVec[1] #kfwd
     paramvec[5] = kfbnd * 10.0 #k1rev
     paramvec[6] = kfbnd * 144.0 #k2rev
-    paramvec[7] = unkVec[2] #k4rev
-    paramvec[8] = unkVec[3] #k5rev
+    paramvec[7] = farhatVec[2] # unkVec[2] #k4rev
+    paramvec[8] = farhatVec[3]#unkVec[3] #k5rev
     paramvec[9] = 12.0 * paramvec[8] / 1.5 #k10
     paramvec[10] = 63.0 * paramvec[8] / 1.5 #k11
     paramvec[11] = kfbnd * 0.065 #k13
     paramvec[12] = kfbnd * 438.0 #k14
-    paramvec[13:16] = unkVec[4:7] #k15, k16, k17, k22
-    paramvec[17] = kfbnd * 59.0 #k23
-    paramvec[18] = unkVec[8] #k25
-    paramvec[19] = 5.0 #endoadjust
-    ke = unkVec[9]
-    sortF = tanh(unkVec[11]) * 0.95 + 0.01
-    krec = unkVec[12]
-    kdeg = unkVec[13]
+    paramvec[13:16] = farhatVec[4:7]# unkVec[4:7] #k16, k17, k22, k23
+    paramvec[17] = kfbnd * 59.0 #k25
+    paramvec[18] = farhatVec[8]#unkVec[8]  #k27
+    paramvec[19] = 5.0 #endoadjust #make sure this is right
+    ke = farhatVec[9]#unkVec[9] 
+    aendo = farhatVec[10]#unkVec[10]
+    sortF = farhatVec[11]# tanh(unkVec[11]) * 0.95 + 0.01
+    krec = farhatVec[12]#unkVec[12]
+    kdeg = farhatVec[13] #unkVec[13]
     paramvec[20] = ke
-    paramvec[21] = unkVec[10]
+    paramvec[21] = aendo
     paramvec[22] = sortF
     paramvec[23] = krec
     paramvec[24] = kdeg
     paramvec[25:29] = recAbundances * ke / (1.0 + (krec * (1.0 - sortF) / kdeg / sortF))
     if CellType == "Treg"
-        paramvec[30] = unkVec[14] #initial stat
+        paramvec[30] = unkVec[1] #initial stat
     elseif CellType == "Thelper"
-        paramvec[30] = unkVec[15] #initial stat
+        paramvec[30] = unkVec[2] #initial stat
     elseif CellType == "NK"
-        paramvec[30] = unkVec[16] #initial stat
+        paramvec[30] = unkVec[3] #initial stat
     elseif CellType == "CD8"
-        paramvec[30] = unkVec[17] #initial stat
+        paramvec[30] = unkVec[4] #initial stat
     else
         @assert false
     end
-    paramvec[31:36] = unkVec[18:23]
+    paramvec[31:36] = unkVec[5:10]
 
     return paramvec
 end
@@ -73,7 +75,8 @@ end
 """ Adjusts the binding activity of ligand to match that of mutein """
 function mutAffAdjust(p::Vector{T}, dfRow) where {T}
     p[5] = dfRow.IL2RaKD[1] * 0.6
-    p[6:10] .*= p[5] / p[8]
+    bgAdjust = (dfRow.IL2RBGKD[1] * 0.6) / p[8]
+    p[6:10] .*= bgAdjust
     return p
 end
 
@@ -82,6 +85,7 @@ end
 function resids(x::Vector{T})::T where {T}
     @assert all(x .>= 0.0)
     df = importData(true)
+    df = df[df.Ligand .!= "IL15", :]
 
     sort!(df, :Time)
     df.Time *= 60.0
@@ -103,8 +107,7 @@ function resids(x::Vector{T})::T where {T}
                 idxx = findfirst(df.Cell .== cell)
                 recpE = 10.0 .^ Vector{Float64}(df[idxx, [:IL15Ra, :IL2Ra, :IL2Rb, :IL7Ra, :gc]])
                 vector = vec(fitParams(ligVec, x, recpE, cell))
-
-                if ligand != "IL15"
+                if ligand !== "IL15" && ligand !== "IL2"
                     vector = mutAffAdjust(vector, df[findfirst(df.Ligand .== ligand), [:IL2RaKD, :IL2RBGKD]])
                 end
                 idxs = (df.Dose .== dose) .& (df.Ligand .== ligand) .& (df.Cell .== cell)
@@ -141,7 +144,7 @@ end
 
 """ Gets inital unkowns, optimizes them, and returns parameters of best fit"""
 function runFit(; itern = 1000000)
-    x₀ = invsoftplus.(getUnkVec())
+    x₀ = invsoftplus.(getUnkVec()[14:23])
 
     opts = Optim.Options(iterations = itern, show_trace = true, extended_trace = true, allow_f_increases = true)
     lsi = InitialStatic(; alpha = 0.0001)
